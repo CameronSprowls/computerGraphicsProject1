f00d81cd91fa29519767426d4dbc7a94f4e0f2f8
Dustin Thurston <dthurston1996@gmail.com>
2018-02-27 19:24:50 -0500

    Clock tower is done

--- geometry/ClockTower.js
+++ geometry/ClockTower.js
@@ -12,8 +12,8 @@ class ClockTower extends ObjectGroup {
             bottomRadius: 0.2,
             numSides: 4,
             height: 0.7,
-            topColor: vec3.fromValues(216/255,39/255,50/255),
-            bottomColor: vec3.fromValues(155/255,38/255,46/255)
+            topColor: vec3.fromValues(170/255,82/255,68/255),
+            bottomColor: vec3.fromValues(167/255,64/255,55/255)
         });
 
         let prism1 = new PolygonalPrism(gl, {
@@ -21,8 +21,8 @@ class ClockTower extends ObjectGroup {
             bottomRadius: 0.07,
             numSides: 4,
             height: 1.0,
-            topColor: vec3.fromValues(216/255,39/255,50/255),
-            bottomColor: vec3.fromValues(155/255,38/255,46/255)
+            topColor: vec3.fromValues(170/255,82/255,68/255),
+            bottomColor: vec3.fromValues(167/255,64/255,55/255)
         });
         mat4.translate(prism1.coordFrame, prism1.coordFrame, vec3.fromValues(0.15,0, 0));
 
@@ -31,8 +31,8 @@ class ClockTower extends ObjectGroup {
             bottomRadius: 0.07,
             numSides: 4,
             height: 1.0,
-            topColor: vec3.fromValues(216/255,39/255,50/255),
-            bottomColor: vec3.fromValues(155/255,38/255,46/255)
+            topColor: vec3.fromValues(170/255,82/255,68/255),
+            bottomColor: vec3.fromValues(167/255,64/255,55/255)
         });
         mat4.translate(prism2.coordFrame,prism2.coordFrame, vec3.fromValues(-0.15,0,0));
 
@@ -42,8 +42,8 @@ class ClockTower extends ObjectGroup {
             bottomRadius: 0.07,
             numSides: 4,
             height: 1.0,
-            topColor: vec3.fromValues(216/255,39/255,50/255),
-            bottomColor: vec3.fromValues(155/255,38/255,46/255)
+            topColor: vec3.fromValues(170/255,82/255,68/255),
+            bottomColor: vec3.fromValues(167/255,64/255,55/255)
         });
         mat4.translate(prism3.coordFrame,prism3.coordFrame,vec3.fromValues(0,0.15,0));
 
@@ -52,8 +52,8 @@ class ClockTower extends ObjectGroup {
             bottomRadius: 0.07,
             numSides: 4,
             height: 1.0,
-            topColor: vec3.fromValues(216/255,39/255,50/255),
-            bottomColor: vec3.fromValues(155/255,38/255,46/255)
+            topColor: vec3.fromValues(170/255,82/255,68/255),
+            bottomColor: vec3.fromValues(167/255,64/255,55/255)
         });
         mat4.translate(prism4.coordFrame,prism4.coordFrame, vec3.fromValues(0,-0.15,0));
 
@@ -62,8 +62,8 @@ class ClockTower extends ObjectGroup {
             bottomRadius: 0.22,
             numSides: 4,
             height: 0.1,
-            topColor: vec3.fromValues(216/255,39/255,50/255),
-            bottomColor: vec3.fromValues(155/255,38/255,46/255)
+            topColor: vec3.fromValues(170/255,82/255,68/255),
+            bottomColor: vec3.fromValues(167/255,64/255,55/255)
         });
         mat4.translate(top.coordFrame,top.coordFrame,vec3.fromValues(0,0,1.0));
 
@@ -72,15 +72,66 @@ class ClockTower extends ObjectGroup {
             bottomRadius: 0.2,
             numSides: 4,
             height: 0.2,
-            topColor: vec3.fromValues(249/255,237/255,238/255),
-            bottomColor: vec3.fromValues(219/255,208/255,209/255)
+            topColor: vec3.fromValues(215/255,220/255,224/255),
+            bottomColor: vec3.fromValues(204/255,209/255,213/255)
         });
+
         mat4.translate(clockPart.coordFrame,clockPart.coordFrame,vec3.fromValues(0,0,1.1));
+
+        let hat = new ObjectGroup(gl);
+        this.group.push(hat);
+
+        let cyl1 = new PolygonalPrism(gl, {
+            topRadius: .14,
+            bottomRadius: .14,
+            numSides: 20,
+            height: .277,
+            topColor: vec3.fromValues(81/255,118/255,118/255),
+            bottomColor: vec3.fromValues(69/255,99/255,97/255)
+        });
+
+        let cyl2 = new PolygonalPrism(gl, {
+            topRadius: .14,
+            bottomRadius: .14,
+            numSides: 20,
+            height: .277,
+            topColor: vec3.fromValues(81/255,118/255,118/255),
+            bottomColor: vec3.fromValues(69/255,99/255,97/255)
+        });
+
+        let hatCone = new Cone(gl,{
+            radius: 0.01,
+            height: 0.3,
+            tipColor: vec3.fromValues(81/255,118/255,118/255),
+            baseColor: vec3.fromValues(69/255,99/255,97/255),
+            radialDiv: 20
+        });
+
+        let hatSphere = new Sphere(gl,0.01,5, vec3.fromValues(69/255,99/255,97/255));
+
+        hat.group.push(cyl1);
+        hat.group.push(cyl2);
+        hat.group.push(hatCone);
+        hat.group.push(hatSphere);
+
+        mat4.translate(hatSphere.coordFrame,hatSphere.coordFrame,vec3.fromValues(0,0,0.22));
+
+        //NOTE: have to rotate around x and y axis, because x/y plane is diagonal
+        mat4.rotateX(cyl1.coordFrame,cyl1.coordFrame,glMatrix.toRadian(90));
+        mat4.rotateY(cyl1.coordFrame, cyl1.coordFrame,glMatrix.toRadian(45));
+        mat4.translate(cyl1.coordFrame,cyl1.coordFrame,vec3.fromValues(0,-0.01,-0.14));
+
+        mat4.rotateX(cyl2.coordFrame,cyl2.coordFrame,glMatrix.toRadian(-90));
+        mat4.rotateY(cyl2.coordFrame, cyl2.coordFrame,glMatrix.toRadian(45));
+        mat4.translate(cyl2.coordFrame,cyl2.coordFrame,vec3.fromValues(0,0.01,-0.14));
+
+        mat4.translate(hat.coordFrame,hat.coordFrame, vec3.fromValues(0,0,1.3));
 
         this.group.push(base);
         this.group.push(prism1,prism2,prism3,prism4);
         this.group.push(top);
         this.group.push(clockPart);
+        this.group.push(hat);
 
     }
 }
--- geometry/Sphere.js
+++ geometry/Sphere.js
@@ -1,124 +1,131 @@
 /**
  * Created by Hans Dulimarta on 2/1/17.
  *
- * Edited by Cameron Sprowls
- *
- * Implemented in a way that is certainly sub-par, however, it works after about 5 hours so I'm
- * going to leave it for now.
+ * Modified by Dustin Thurston 2/14/18
  */
-
 class Sphere extends Object3D {
-  /**
-   * Create a 3D sphere with tip at the Z+ axis and base on the XY plane
-   * @param {Object} gl      the current WebGL context
-   * @param {Number} radius  radius of the sphere
-   * @param {Number} subDiv  number of recursive subdivisions
-   * @param {vec3}   col1    color #1 to use
-   */
-    constructor (gl, radius, subDiv, col1) {
+    /**
+     * Create a 3D sphere with tip at the Z+ axis and base on the XY plane
+     * @param {Object} gl      the current WebGL context
+     * @param {Number} RADIUS  radius of the sphere
+     * @param {Number} subDiv number of recursive subdivisions
+     * @param {vec3}   [col1]    color #1 to use
+     */
+    constructor (gl, RADIUS, subDiv, col1) {
         super(gl);
+        /* if colors are undefined, generate random colors */
+        if (typeof col1 === "undefined") col1 = vec3.fromValues(Math.random(), Math.random(), Math.random());
 
-        // Sphere essentials
+        this.RADIUS = RADIUS;
+        let seedA = vec3.fromValues(1, 1, 1);
+        vec3.normalize(seedA, seedA);
+        vec3.scale (seedA, seedA, RADIUS);
+        let seedB = vec3.fromValues(-1, -1, 1);
+        vec3.normalize(seedB, seedB);
+        vec3.scale (seedB, seedB, RADIUS);
+        let seedC = vec3.fromValues(-1, 1, -1);
+        vec3.normalize(seedC, seedC);
+        vec3.scale (seedC, seedC, RADIUS);
+        let seedD = vec3.fromValues(1, -1, -1);
+        vec3.normalize(seedD, seedD);
+        vec3.scale (seedD, seedD, RADIUS);
+
+        /* TODO: complete the rest of the code here */
+
         this.vertices = [];
         this.indices = [];
         this.colors = [];
-        this.radius = radius;
 
-        // if colors are undefined, generate random colors
-        if (typeof col1 === "undefined") {
-            this.col1 = vec3.fromValues(Math.random(), Math.random(), Math.random());
+        //recursive calls
+        this.subDivide(seedA, seedC, seedB, subDiv);
+        this.subDivide(seedD, seedA, seedB, subDiv);
+        this.subDivide(seedD, seedC, seedA, subDiv);
+        this.subDivide(seedB, seedC, seedD, subDiv);
+
+        let color = Math.random();
+        //loop to push random colors to the color array
+        for(let x=0; x< this.vertices.length; x++) {
+            this.colors.push(col1[0], col1[1], col1[2]);
         }
 
-        let seedA = vec3.fromValues(1, 1, 1);
-        vec3.normalize(seedA, seedA);
-        vec3.scale (seedA, seedA, radius);
-
-        let seedB = vec3.fromValues(-1, -1, 1);
-        vec3.normalize(seedB, seedB);
-        vec3.scale (seedB, seedB, radius);
-
-        let seedC = vec3.fromValues(-1, 1, -1);
-        vec3.normalize(seedC, seedC);
-        vec3.scale (seedC, seedC, radius);
-
-        let seedD = vec3.fromValues(1, -1, -1);
-        vec3.normalize(seedD, seedD);
-        vec3.scale (seedD, seedD, radius);
-
-        // Call to subdivide triangles, done in such a way to always be counter clockwise
-        this.subDivideCircle(seedC, seedB, seedA, subDiv);
-        this.subDivideCircle(seedD, seedA, seedB, subDiv);
-        this.subDivideCircle(seedA, seedD, seedC, subDiv);
-        this.subDivideCircle(seedB, seedC, seedD, subDiv);
-
-        // Populate indices
-        for (let i = 0; i < this.vertices.length; i++){
-            this.indices.push(i);
+        //push to the index array
+        for(let x = 0; x < this.vertices.length; x++){
+            this.indices.push(x);
         }
         this.indices.push(0);
 
-        // Create gl buffers and draw them
+        console.log(this.indices);
+        console.log(this.vertices);
+
         this.colorBuff = gl.createBuffer();
         gl.bindBuffer(gl.ARRAY_BUFFER, this.colorBuff);
         gl.bufferData(gl.ARRAY_BUFFER, Float32Array.from(this.colors), gl.STATIC_DRAW);
 
+
         gl.bindBuffer(gl.ARRAY_BUFFER, this.vertexBuff);
         gl.bufferData(gl.ARRAY_BUFFER, Float32Array.from(this.vertices), gl.STATIC_DRAW);
 
-        this.indBuff = gl.createBuffer();
-        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.indBuff);
+        let buff = gl.createBuffer();
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, buff);
         gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, Uint16Array.from(this.indices), gl.STATIC_DRAW);
-        this.primitives.push({type: gl.TRIANGLES, buffer: this.indBuff, numPoints: this.indices.length/3});
-  }
+        this.primitives.push({type: gl.TRIANGLES, buffer: buff, numPoints: this.indices.length/3});
 
-    /***
-     * Divides a given triangle into 4 smaller triangles,
-     * with midpoints and scales them to the proper size.
-     * @param point1 first point of the triangle.
-     * @param point2 second point of the triangle.
-     * @param point3 third point of the triangle.
-     * @param subDiv the amount of times the triangle is to be divided.
-     *               Uses recursion, be careful when passing in a
-     *               large value to avoid that fabled stack overflow.
-     ***/
-    subDivideCircle(point1, point2, point3, subDiv) {
-        if (subDiv > 0) {
+    }
 
-            let midpoint1 = vec3.create();
-            let midpoint2 = vec3.create();
-            let midpoint3 = vec3.create();
+    subDivide(seed1, seed2, seed3, subDiv){
 
-            // Get the midpoints of all the vectors, normalize, and scale them
-            vec3.add(midpoint1, point1, point2);
-            midpoint1 = vec3.fromValues(midpoint1[0]/2, midpoint1[1]/2, midpoint1[2]/2);
-            vec3.normalize(midpoint1, midpoint1);
-            vec3.scale(midpoint1, midpoint1, this.radius);
+        if(subDiv > 0) {
 
-            vec3.add(midpoint2, point2, point3);
-            midpoint2 = vec3.fromValues(midpoint2[0]/2, midpoint2[1]/2, midpoint2[2]/2);
-            vec3.normalize(midpoint2, midpoint2);
-            vec3.scale(midpoint2, midpoint2, this.radius);
+            //midpoint1 between A and B
+            let mp1x = (seed1[0] + seed2[0]) / 2;
+            let mp1y = (seed1[1] + seed2[1]) / 2;
+            let mp1z = (seed1[2] + seed2[2]) / 2;
 
-            vec3.add(midpoint3, point1, point3);
-            midpoint3 = vec3.fromValues(midpoint3[0]/2, midpoint3[1]/2, midpoint3[2]/2);
-            vec3.normalize(midpoint3, midpoint3);
-            vec3.scale(midpoint3, midpoint3, this.radius);
 
-            // Recursive call, done in such a way to always be counter clockwise
-            this.subDivideCircle(point1, midpoint1, midpoint3, subDiv-1);
-            this.subDivideCircle(midpoint1, point2, midpoint2, subDiv-1);
-            this.subDivideCircle(midpoint3, midpoint2, point3, subDiv-1);
-            this.subDivideCircle(midpoint2, midpoint3, midpoint1, subDiv-1);
+            let mp1 = vec3.fromValues(mp1x, mp1y, mp1z);
+            vec3.normalize(mp1, mp1);
+            vec3.scale(mp1, mp1, this.RADIUS);
 
-        } else {
-            // Adds the final triangle to the vertices to be drawn, also adds color here so the triangle is
-            // all the same color
-            this.vertices.push(point1[0], point1[1], point1[2]);
-            this.colors.push(this.col1[0], this.col1[1], this.col1[2]);
-            this.vertices.push(point2[0], point2[1], point2[2]);
-            this.colors.push(this.col1[0], this.col1[1], this.col1[2]);
-            this.vertices.push(point3[0], point3[1], point3[2]);
-            this.colors.push(this.col1[0], this.col1[1], this.col1[2]);
+            //this.vertices.push(mp1[0],mp1[1],mp1[2]);
+
+            //midpoint 2 between B and C
+            let mp2x = (seed2[0] + seed3[0]) / 2;
+            let mp2y = (seed2[1] + seed3[1]) / 2;
+            let mp2z = (seed2[2] + seed3[2]) / 2;
+
+
+            let mp2 = vec3.fromValues(mp2x, mp2y, mp2z);
+            vec3.normalize(mp2, mp2);
+            vec3.scale(mp2, mp2, this.RADIUS);
+
+            //this.vertices.push(mp2[0],mp2[1],mp2[2]);
+
+            //midpoint 3 between A and C
+            let mp3x = (seed1[0] + seed3[0]) / 2;
+            let mp3y = (seed1[1] + seed3[1]) / 2;
+            let mp3z = (seed1[2] + seed3[2]) / 2;
+
+
+            let mp3 = vec3.fromValues(mp3x, mp3y, mp3z);
+            vec3.normalize(mp3, mp3);
+            vec3.scale(mp3, mp3, this.RADIUS);
+
+            //this.vertices.push(mp3[0],mp3[1],mp3[2]);
+
+            //make sure it's counter clockwise
+            this.subDivide(seed1,mp1,mp3, subDiv-1);
+            this.subDivide(mp1,seed2,mp2, subDiv-1);
+            this.subDivide(mp3,mp2,seed3, subDiv-1);
+            this.subDivide(mp2,mp3,mp1, subDiv-1);
+
+        }else{
+
+            //push the vertices
+            this.vertices.push(seed1[0], seed1[1], seed1[2]);
+            this.vertices.push(seed2[0], seed2[1], seed2[2]);
+            this.vertices.push(seed3[0], seed3[1], seed3[2]);
+
+            return;
         }
     }
 }
